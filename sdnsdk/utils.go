89523b15e799cf773a804873320bfa98ab76516a
Bogdan Prodan <bogdan.prodan.j@gmail.com>
2026-04-07 09:22:15 +0300

    Fix update cached file

--- sdnsdk/utils.go
+++ sdnsdk/utils.go
@@ -1,11 +1,9 @@
 package sdnsdk
 
 import (
-	"bufio"
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"path"
@@ -19,30 +17,12 @@ var ErrMalformedAuthHeader = errors.New("auth header is not in the correct forma
 // UpdateCacheFile - update a cache file
 func UpdateCacheFile(dataDir string, fileName string, value []byte) error {
 	cacheFileName := path.Join(dataDir, fileName)
-	f, err := os.OpenFile(cacheFileName, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	writer := bufio.NewWriter(f)
-	_, err = writer.Write(value)
-	if err != nil {
-		return err
-	}
-
-	return writer.Flush()
+	return os.WriteFile(cacheFileName, value, 0644)
 }
 
 // LoadCacheFile - load a cache file
 func LoadCacheFile(dataDir string, fileName string) ([]byte, error) {
-	cacheFileName := path.Join(dataDir, fileName)
-	f, err := os.Open(cacheFileName)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	return io.ReadAll(bufio.NewReader(f))
+	return os.ReadFile(path.Join(dataDir, fileName))
 }
 
 // GetIP checks the existence of and returns the IP address for a host name
